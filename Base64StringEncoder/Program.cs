0ccd36dcc8030e216bb04087ae917e4c4a2864f5
cfenwick <carolyn.fenwick@gemalto.com>
2019-03-06 15:28:20 -0500

    Initial commit

---
+++ Base64StringEncoder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Base64StringEncoder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "input";
+            Console.WriteLine("This program allows you to base64 encode strings. Enter 0 when done.");
+            Console.WriteLine();
+            while (!input.Equals("0"))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Enter secret to encode or 0 to exit. ");
+                Console.WriteLine();
+
+                input = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(input) && !input.EndsWith("0"))
+                    Console.WriteLine(GetEncodedString(input));
+                Console.WriteLine();
+            }
+            Console.WriteLine("Press enter to quit.");
+            Console.ReadLine();
+        }
+
+
+
+        public static string GetDecodedString(string input)
+        {
+            return System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(input));
+        }
+
+        public static string GetEncodedString(string input)
+        {
+            return Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(input));
+        }
+    }
+}
